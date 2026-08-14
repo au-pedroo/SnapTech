d3db4021e549fab410551376cf5004d1c512a739
Pedro Augusto <pedro.augustinho.marinho@hotmail.com>
2026-08-13 21:01:59 -0300

    Criação do Projeto

---
+++ src/main/java/com/example/SnapTech/SnapTechApplication.java
@@ -0,0 +1,13 @@
+package com.example.SnapTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SnapTech/SnapTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SnapTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
